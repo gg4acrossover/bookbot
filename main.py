279841bc8543dfa84289192c32ecf7b82008d5f3
viethq <gg4acrossover@gmail.com>
2024-01-18 16:15:01 +0700

    step 16

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ PATH_TO_FILE = "books/Frankenstein.txt"
 
 def main():
     content = get_file_contents(PATH_TO_FILE)
-    print(content)
-    print(f"get words count: {get_word_count(content)}")
-    print(f"get char count: {get_char_count(content)}")
+    report(content)
+
+def report(content):
+    print(f"--- Begin report of {PATH_TO_FILE} ---")
+    print(f"{get_word_count(content)} words found in the document")
+    print_all_element_count(get_char_count(content))
+    print("--- End report ---")
 
 def get_file_contents(path_to_file):
     file_contents = ""
@@ -21,7 +25,7 @@ def get_word_count(words):
 def get_char_count(words):
     dict = {}
     letters = set(string.ascii_lowercase)
-    print(f"letter {letters}")
+    # print(f"letter {letters}")
     for c in letters:
         dict[c] = 0
     
@@ -31,5 +35,18 @@ def get_char_count(words):
             dict[c_lower] += 1
     
     return dict
+
+def print_all_element_count(char_dict):
+    list = []
+    for key, value in char_dict.items():
+        list.append((key, value))
+    list.sort(key=lambda a: a[1])
+
+    # print(f"list count {len(list)}")
+    
+    # reverse index and print all values
+    for idx in range(len(list)-1,-1,-1):
+        item = list[idx]
+        print(f"The '{item[0]}' character was found {item[1]} times")
 
 main()
